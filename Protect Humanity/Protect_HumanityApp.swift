e1c969e726a45cb743a6b01b40e35528b3fda88b
Conner Yoon <con.yoon@icloud.com>
2022-06-15 17:06:46 -0400

    Initial Commit

---
+++ Protect Humanity/Protect_HumanityApp.swift
@@ -0,0 +1,17 @@
+//
+//  Protect_HumanityApp.swift
+//  Protect Humanity
+//
+//  Created by Conner Yoon on 6/15/22.
+//
+
+import SwiftUI
+
+@main
+struct Protect_HumanityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
